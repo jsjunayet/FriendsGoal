0fd2964e52b267e4378d5d3dcea5a80d794b9835
Junayet Shiblu <130874714+jsjunayet@users.noreply.github.com>
2026-07-18 08:49:25 +0600

    fix: updated configuration and fixed typescript compilation errors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import cors from "cors";
 import dotenv from "dotenv";
-import express, { Request, Response } from "express";
+import type { Request, Response } from "express";
+import express from "express";
 
 dotenv.config();
 
